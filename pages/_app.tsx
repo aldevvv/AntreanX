3b57570add658d527a2b6b79439c8edabd52081f
Muhammad Alif <mhdalif.id@gmail.com>
2025-07-17 09:59:22 +0800

    Update Site Name & Icon

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,20 @@
 // pages/_app.tsx
 import { SessionProvider } from "next-auth/react";
 import type { AppProps } from "next/app";
+import Head from "next/head";
 import "@/styles/globals.css"; // atau "../styles/globals.css"
 
 
 export default function App({ Component, pageProps: { session, ...pageProps } }: AppProps) {
   return (
-    <SessionProvider session={session}>
-      <Component {...pageProps} />
-    </SessionProvider>
+    <>
+      <Head>
+        <title>AntreanX</title>
+        <link rel="icon" href="/Indibiz.png" />
+      </Head>
+      <SessionProvider session={session}>
+        <Component {...pageProps} />
+      </SessionProvider>
+    </>
   );
 }
